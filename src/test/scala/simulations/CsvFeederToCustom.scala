56752c1996a8c7011b214aa0fe0b0898feafe13c
Rafal Lesnicki <lesnickirafal@gmail.com>
2022-04-27 12:12:48 +0200

    tests

---
+++ src/test/scala/simulations/CsvFeederToCustom.scala
@@ -0,0 +1,34 @@
+package simulations
+
+import io.gatling.core.Predef._
+import io.gatling.core.scenario.Simulation
+import io.gatling.http.Predef._
+
+class CsvFeederToCustom extends Simulation {
+
+  val httpConf = http.baseUrl("http://localhost:8082/app/")
+    .header("Accept", "application/json")
+    .proxy(Proxy("localhost", 8088))
+
+  var idNumbers = (1 to 10).iterator
+
+  val customFeeder = Iterator.continually(Map("gameId" -> idNumbers.next()))
+
+  def getSpecificVideoGame() = {
+    repeat(10) {
+      feed(customFeeder)
+        .exec(http("Get specific video game")
+          .get("videogames/${gameId}")
+          .check(status.is(200)))
+        .pause(1)
+    }
+  }
+
+  val scn = scenario("Csv Feeder test")
+    .exec(getSpecificVideoGame())
+
+  setUp(
+    scn.inject(atOnceUsers(1))
+  ).protocols(httpConf)
+
+}
